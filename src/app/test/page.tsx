9688563a9717a6ea1a58a11a226540a415621206
Sailendra Darbha <sailendra.darbha@gmail.com>
2024-01-19 02:12:42 +0530

    incremental but minor changes done

---
+++ src/app/test/page.tsx
@@ -0,0 +1,28 @@
+"use client"
+import React from "react";
+import { Select, SelectItem } from "@nextui-org/react";
+import { animals } from "../../constants/data";
+export default function Page() {
+  return (
+    <div className="flex w-full min-h-screen flex-wrap gap-4">
+      <Select label="Select an animal" className="max-w-xs">
+        {animals.map((animal) => (
+          <SelectItem key={animal.value} value={animal.value} className="text-black">
+            {animal.label}
+          </SelectItem>
+        ))}
+      </Select>
+      <Select
+        label="Favorite Animal"
+        placeholder="Select an animal"
+        className="max-w-xs"
+      >
+        {animals.map((animal) => (
+          <SelectItem key={animal.value} value={animal.value}>
+            {animal.label}
+          </SelectItem>
+        ))}
+      </Select>
+    </div>
+  );
+}
