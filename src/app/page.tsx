ff6118bfee478282f197ff27e00ba87c6fd84878
SailendraDarbha94 <sailendra.darbha@gmail.com>
2024-04-15 17:32:49 +0530

    homepage done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,14 @@ import ExpandingTriangles from "@/components/ui/ExpandingTriangles";
 import { Button, Divider } from "@nextui-org/react";
 import { useEffect, useRef, useState } from "react";
 import BasicCard from "@/components/BasicCard";
+import NavBarCallLink from "@/components/NavBarCallLink";
+export interface CardProps {
+  id: number;
+  url: string;
+  alt: string;
+  text: string;
+  btn: string;
+}
 
 export default function Home() {
   const toggleButton = (el: any) => {
@@ -35,49 +43,53 @@ export default function Home() {
 
   useEffect(() => {
     fetchQuote();
-  },[]);
+  }, []);
 
   const buttonRef = useRef<HTMLButtonElement>(null);
 
-  const cards:any = [
+  const cards: CardProps[] = [
     {
       id: 1,
-      title: "Report a Crime",
-      subtitle: "Report a crime anonymously",
-      description: "Report a crime anonymously",
-      urllink: "/report",
-      urltext: "Report a Crime"
+      url: "/plum.png",
+      alt: "plums",
+      text: "Are you Suspicious?",
+      btn: "Post a Tip",
     },
     {
       id: 2,
-      title: "Crime Statistics",
-      subtitle: "View Crime Statistics",
-      description: "View Crime Statistics",
-      urllink: "/statistics",
-      urltext: "Crime Statistics"
+      url: "/lemon.png",
+      alt: "lemons",
+      text: "Crime Prevention",
+      btn: "Learn More",
     },
     {
       id: 3,
-      title: "Crime Prevention",
-      subtitle: "Crime Prevention Tips",
-      description: "Crime Prevention Tips",
-      urllink: "/prevention",
-      urltext: "Crime Prevention"
+      url: "/strawberry.png",
+      alt: "strawberries",
+      text: "Safety Tips",
+      btn: "Read More",
+    },
+    {
+      id: 4,
+      url: "/tomato.png",
+      alt: "tomatoes",
+      text: "Report a Crime",
+      btn: "Report Now",
     }
-  ]
+  ];
 
   const toggleNavbar = () => {
-    if(navbarHeight === "h-14") {
-      setNavbarHeight("h-4/6")
+    if (navbarHeight === "h-14") {
+      setNavbarHeight("h-4/6");
     } else {
-      setNavbarHeight("h-14")
+      setNavbarHeight("h-14");
     }
-  }
+  };
   return (
     <main className="flex min-h-screen w-full max-w-screen flex-col p-4 md:p-24">
       <div>
         <h1 className="text-3xl md:text-6xl font-bold text-blue-900">
-          Welcome to CrackCrimeBahamas
+          Welcome to Crack-Crime-Bahamas
         </h1>
         <p className="mt-4 md:ml-3 text-md mb-4">Your safety is our Priority</p>
       </div>
@@ -92,21 +104,39 @@ export default function Home() {
       </div> */}
       <Divider />
       <div className="w-full my-4">
-        <pre className="text-wrap italic font-semibold">{quote ? quote : ""}</pre>
+        <pre className="text-wrap italic font-semibold">
+          {quote ? quote : ""}
+        </pre>
         <pre className="text-right">{author ? "-" + author : ""}</pre>
       </div>
       <Divider />
-      <div className="flex w-full bg-lime-300 flex-wrap items-center">
-        {cards.map((card:any, index:number) => {
+      <div className="flex w-fullmin-h-fit flex-wrap justify-evenly items-center">
+        {cards.map((card: any, index: number) => {
           return (
-            <div className="lg:w-2/6 md:w-3/6 w-5/6 mx-auto" key={card.id}>
-              <BasicCard {...card} />
-            </div>
-          )
+              <BasicCard key={card.id} {...card} />
+          );
         })}
       </div>
-      <div className={`min-w-screen w-full z-50 transition-height fixed bottom-0 left-0 bg-slate-300 p-2 hover:cursor-pointer ${navbarHeight}`} onClick={toggleNavbar}>
-        navbar
+      <div
+        className={`min-w-screen flex flex-col w-full z-50 transition-height fixed bottom-0 left-0 bg-slate-300 p-2 hover:cursor-pointer ${navbarHeight}`}
+      >
+        <Button
+          radius="full"
+          className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg mx-auto"
+          onPress={toggleNavbar}
+        >
+          {navbarHeight === "h-14" ? "Show More" : "Show Less"}
+        </Button>
+        <div
+          className={`${
+            navbarHeight === "h-14" ? "hidden" : "block"
+          } flex flex-col`}
+        >
+          <NavBarCallLink title="Emergency" number="911" />
+          <NavBarCallLink title="Family Islands" number="242-300-8477" />
+          <NavBarCallLink title="Nassau" number="328-8477" />
+          <NavBarCallLink title="Help" number="919" />
+        </div>
       </div>
     </main>
   );
--- src/components/BasicCard.tsx
+++ src/components/BasicCard.tsx
@@ -1,37 +1,27 @@
 "use client"
 import React from "react";
-import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nextui-org/react";
+import {Card, CardHeader, CardBody, CardFooter, Button, Divider, Link, Image} from "@nextui-org/react";
+import { CardProps } from "@/app/page";
 
-export default function BasicCard({title, subtitle, description, urllink, urltext}:any) {
+export default function BasicCard({url, alt, text, btn}:CardProps) {
   return (
-    <Card className="m-2 flex">
-      <CardHeader className="flex gap-3">
-        <Image
-          alt="nextui logo"
-          height={40}
-          radius="sm"
-          src="https://avatars.githubusercontent.com/u/86160567?s=200&v=4"
-          width={40}
-        />
-        <div className="flex flex-col">
-          <p className="text-md">{title}</p>
-          <p className="text-small text-default-500">{subtitle}</p>
-        </div>
-      </CardHeader>
-      <Divider/>
-      <CardBody>
-        <p>{description}</p>
-      </CardBody>
-      <Divider/>
-      <CardFooter>
-        <Link
-          isExternal
-          showAnchorIcon
-          href={urllink}
-          className="mx-auto"
-        >
-          {urltext}
-        </Link>
+    <Card
+      isFooterBlurred
+      radius="lg"
+      className="border-none min-h-60 min-w-60 m-2"
+    >
+      <Image
+        alt={alt}
+        className="object-cover"
+        height={200}
+        src={url}
+        width={200}
+      />
+      <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+        <p className="text-tiny">{text}</p>
+        <Button className="text-tiny text-white bg-black/80" variant="flat" color="default" radius="lg" size="sm">
+          {btn}
+        </Button>
       </CardFooter>
     </Card>
   );
