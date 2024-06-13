75e2404ec2e9b4f8ce84da39483ddd59e57ef0bf
SailendraDarbha94 <sailendra.darbha@gmail.com>
2024-06-13 19:13:16 +0530

    security

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -9,14 +9,14 @@ import { getStorage } from "firebase/storage";
 import { getAnalytics } from "firebase/analytics";
 // Initialize Firebase
 const firebaseConfig = {
-    apiKey: "AIzaSyBVUwTCTvah5wRiiIxDun8YpMfeRYaQPqo",
-    authDomain: "amplify-dentistry-9c178.firebaseapp.com",
-    projectId: "amplify-dentistry-9c178",
-    storageBucket: "amplify-dentistry-9c178.appspot.com",
-    messagingSenderId: "1029656965103",
-    databaseURL: "https://amplify-dentistry-9c178-default-rtdb.asia-southeast1.firebasedatabase.app/",
-    appId: "1:1029656965103:web:eac462a52a6b4c78aa101a",
-    measurementId: "G-TYSNZVGY45"
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID
   };
   
 // if (!firebase.apps.length) {
