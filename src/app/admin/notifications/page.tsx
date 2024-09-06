a46416ab535ba4d6060661970aeed777780c7091
SailendraDarbha94 <sailendra.darbha@gmail.com>
2024-09-06 16:12:04 +0530

    notifications backend init

---
+++ src/app/admin/notifications/page.tsx
@@ -0,0 +1,145 @@
+"use client";
+import { Expo } from "expo-server-sdk";
+import { useState } from "react";
+const Page = () => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const somePushTokens = [`ExponentPushToken[${process.env.NEXT_PUBLIC_TEST_EXPONENT_TOKEN}]`];
+  let expo = new Expo({
+    accessToken: process.env.NEXT_PUBLIC_EXPO_ACCESS_TOKEN,
+    useFcmV1: false, // this can be set to true in order to use the FCM v1 API
+  });
+
+  // Create the messages that you want to send to clients
+  let messages: any = [];
+  for (let pushToken of somePushTokens) {
+    // Each push token looks like ExponentPushToken[xxxxxxxxxxxxxxxxxxxxxx]
+
+    // Check that all your push tokens appear to be valid Expo push tokens
+    if (!Expo.isExpoPushToken(pushToken)) {
+      console.error(`Push token ${pushToken} is not a valid Expo push token`);
+      continue;
+    }
+
+    // Construct a message (see https://docs.expo.io/push-notifications/sending-notifications/)
+    messages.push({
+      to: pushToken,
+      sound: "default",
+      body: "This is a test notification",
+      data: { withSome: "data" },
+    });
+  }
+
+  let chunks = expo.chunkPushNotifications(messages);
+  let tickets: any = [];
+  (async () => {
+    // Send the chunks to the Expo push notification service. There are
+    // different strategies you could use. A simple one is to send one chunk at a
+    // time, which nicely spreads the load out over time:
+    for (let chunk of chunks) {
+      try {
+        let ticketChunk = await expo.sendPushNotificationsAsync(chunk);
+        console.log(ticketChunk);
+        tickets.push(...ticketChunk);
+        // NOTE: If a ticket contains an error code in ticket.details.error, you
+        // must handle it appropriately. The error codes are listed in the Expo
+        // documentation:
+        // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  })();
+
+  // Later, after the Expo push notification service has delivered the
+  // notifications to Apple or Google (usually quickly, but allow the service
+  // up to 30 minutes when under load), a "receipt" for each notification is
+  // created. The receipts will be available for at least a day; stale receipts
+  // are deleted.
+  //
+  // The ID of each receipt is sent back in the response "ticket" for each
+  // notification. In summary, sending a notification produces a ticket, which
+  // contains a receipt ID you later use to get the receipt.
+  //
+  // The receipts may contain error codes to which you must respond. In
+  // particular, Apple or Google may block apps that continue to send
+  // notifications to devices that have blocked notifications or have uninstalled
+  // your app. Expo does not control this policy and sends back the feedback from
+  // Apple and Google so you can handle it appropriately.
+  let receiptIds = [];
+  for (let ticket of tickets) {
+    // NOTE: Not all tickets have IDs; for example, tickets for notifications
+    // that could not be enqueued will have error information and no receipt ID.
+    if (ticket.status === "ok") {
+      receiptIds.push(ticket.id);
+    }
+  }
+
+  let receiptIdChunks = expo.chunkPushNotificationReceiptIds(receiptIds);
+  (async () => {
+    // Like sending notifications, there are different strategies you could use
+    // to retrieve batches of receipts from the Expo service.
+    for (let chunk of receiptIdChunks) {
+      try {
+        let receipts = await expo.getPushNotificationReceiptsAsync(chunk);
+        console.log(receipts);
+
+        // The receipts specify whether Apple or Google successfully received the
+        // notification and information about an error, if one occurred.
+        for (let receiptId in receipts) {
+          let { status, details } = receipts[receiptId];
+          //
+          if (status === "ok") {
+            continue;
+          } else if (status === "error") {
+            console.error(`There was an error sending a notification`);
+            if (details) {
+              // The error codes are listed in the Expo documentation:
+              // https://docs.expo.io/push-notifications/sending-notifications/#individual-errors
+              // You must handle the errors appropriately.
+              console.error(`The error code is ${JSON.stringify(details)}`);
+            }
+          }
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  })();
+  return (
+    <div className="w-full min-h-screen">
+      Page Heading
+      {loading ? (
+        <div className="w-full min-h-96 flex justify-center items-center">
+          <div
+            role="status"
+            className="flex
+                    min-h-96
+                    max-h-full
+                    justify-center
+                    items-center"
+          >
+            <svg
+              aria-hidden="true"
+              className="w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+              viewBox="0 0 100 101"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                fill="currentColor"
+              />
+              <path
+                d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                fill="currentFill"
+              />
+            </svg>
+            <span className="sr-only">Loading...</span>
+          </div>
+        </div>
+      ) : null}
+    </div>
+  );
+};
+
+export default Page;
