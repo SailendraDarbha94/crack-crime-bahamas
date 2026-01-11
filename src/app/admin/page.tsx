47ad71496b9496b98ac9c6425fbbffe871453ade
SailendraDarbha94 <sailendra.darbha@gmail.com>
2026-01-11 22:31:41 +0530

    admin dashboard landing page updated

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,13 +1,62 @@
 "use client";
-import app from "@/lib/firebase";
-import { dateReader } from "@/lib/utils";
-import { child, get, getDatabase, ref, remove } from "firebase/database";
+import app, { database } from "@/lib/firebase";
+import { ref, get } from "firebase/database";
+import Image from "next/image";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
 const Page = () => {
   const [loading, setLoading] = useState<boolean>(false);
   const router = useRouter();
+
+  const [numberOfMissings, setNumberOfMissings] = useState<number>(0);
+  const [numberOfWanteds, setNumberOfWanteds] = useState<number>(0);
+  const [numberOfMessages, setNumberOfMessages] = useState<number>(0);
+
+  const gettingNumbers = async () => {
+    setLoading(true);
+    const missingsRef = ref(database, "missings");
+    const wantedsRef = ref(database, "wanteds");
+    const messagesRef = ref(database, "messages");
+    try {
+      const missingsSnapshot = await get(missingsRef);
+      const wantedsSnapshot = await get(wantedsRef);
+      const messagesSnapshot = await get(messagesRef);
+
+      if (!missingsSnapshot.exists()) {
+        console.log("No data available");
+        return;
+      } else {
+        console.log("Number of Missing Persons is ", missingsSnapshot.size)
+        setNumberOfMissings(missingsSnapshot.size);
+      }
+
+      if (!wantedsSnapshot.exists()) {
+        console.log("No data available");
+        return;
+      } else {
+        console.log("Number of Wanted Persons is ", wantedsSnapshot.size)
+        setNumberOfWanteds(wantedsSnapshot.size);
+      }
+
+      if (!messagesSnapshot.exists()) {
+        console.log("No data available");
+        return;
+      } else {
+        console.log("Number of Messages is ", messagesSnapshot.size)
+        setNumberOfMessages(messagesSnapshot.size);
+      }
+
+    } catch (error) {
+      console.error("Error fetching missings data:", error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    gettingNumbers();
+  }, [])
 
   return (
     <div className="min-h-fit font-nunito flex flex-wrap p-4 md:p-14">
@@ -35,13 +84,23 @@ const Page = () => {
           <span className="sr-only">Loading...</span>
         </div>
       ) : (
-        <div className="w-full md:w-2/5 font-nunito text-lg">
-          <h1 className="underline text-xl mb-2">Recent Updates</h1>
-          <ol>
-            <li>Push Notifications</li>
-            {/* <li>Push Notifications for specific device</li> */}
-          </ol>
-            
+        <div className="w-full font-nunito text-lg">
+          <h1 className="underline font-bold text-5xl pb-4 mb-4 text-center">ADMIN DASHBOARD</h1>
+
+          <div className="flex flex-wrap w-full p-4 rounded-lg gap-4">
+            <div className="w-full md:w-1/3 lg:w-1/4 min-h-80 hover:cursor-pointer">
+              <p className="text-center text-3xl font-bold">Missing : {numberOfMissings ? numberOfMissings : "Loading..."}</p>
+              <img src="/thumbnails/missingThumbnail.png" alt="Missing Person Thumbnail" className="rounded-lg" />
+            </div>
+            <div className="w-full md:w-1/3 lg:w-1/4 min-h-80 hover:cursor-pointer">
+              <p className="text-center text-3xl font-bold">Wanted : {numberOfWanteds ? numberOfWanteds : "Loading..."}</p>
+              <img src="/thumbnails/wantedThumbnail3.png" alt="Wanted Person Thumbnail" className="rounded-3xl" />
+            </div>
+            <div className="w-full md:w-1/3 lg:w-1/4 min-h-80 hover:cursor-pointer">
+              <p className="text-center text-3xl font-bold">Tips : {numberOfMessages ? numberOfMessages : "Loading..."}</p>
+              <img src="/thumbnails/tips.png" alt="Messages Thumbnail" className="rounded-xl" />
+            </div>
+          </div>
         </div>
       )}
     </div>
--- src/components/WebNavbar.tsx
+++ src/components/WebNavbar.tsx
@@ -87,7 +87,7 @@ const WebNavbar: React.FC = () => {
     return (
         <nav className="relative">
             {/* Main Navbar */}
-            <div className="mx-2 rounded-3xl bg-gradient-to-r from-amber-500 via-yellow-500 to-amber-300">
+            <div className="mx-4 my-1.5 rounded-3xl bg-gradient-to-r from-amber-500 via-yellow-500 to-amber-300">
                 <div className="px-6 py-4">
                     <div className="flex items-center justify-between">
                         {/* Logo */}
@@ -99,14 +99,14 @@ const WebNavbar: React.FC = () => {
                         {/* Desktop Navigation */}
                         {pathname.includes("admin") ? (
                             <div className="hidden md:flex items-center space-x-3">
-                                <NavLink href="/admin">
+                                {/* <NavLink href="/admin">
                                     <div className="flex items-center gap-3">
                                         <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
                                         </svg>
                                         Home
                                     </div>
-                                </NavLink>
+                                </NavLink> */}
                                 <NavLink href="/admin/profile">
                                     <div className="flex items-center gap-3">
                                         <svg className="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24">
